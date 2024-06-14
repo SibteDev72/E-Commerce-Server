fadb5e82e19b7d30ab38267f93d4f924420c065a
Sibte Muhammad <sibte.dev72@gmail.com>
2024-06-14 20:46:40 +0500

    Made Changes

--- database/Database.js
+++ database/Database.js
@@ -7,14 +7,4 @@ mongoose.connect(process.env.MONGO_DB_URL)
     })
     .catch(() =>{
         console.log("E-Commerce-Project DATABASE IS NOT CONNECTED");
-    })                                                                                                                     
-
-// const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://127.0.0.1:27017/E-Commerce')
-//     .then(() => {
-//         console.log("E-Commerce-Project DATABASE IS CONNECTED SUCCESSFULLY");
-//     })
-//     .catch(() =>{
-//         console.log("E-Commerce-Project DATABASE IS NOT CONNECTED");
-// })                                                                                                                              
+    })                                                                                                                                                                                                                                               
