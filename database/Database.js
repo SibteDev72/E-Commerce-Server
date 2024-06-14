4d90123d6a4fe57ef7360cf127b77b229581480c
Sibte Muhammad <sibte.dev72@gmail.com>
2024-06-14 21:20:50 +0500

    Made Changes DB

--- database/Database.js
+++ database/Database.js
@@ -1,20 +1,10 @@
 const mongoose = require('mongoose');
-// require('dotenv').config()
+require('dotenv').config()
 
-mongoose.connect('mongodb+srv://sibtedev72:commerce@cluster0.8e3txpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGO_DB_URL)
     .then(() => {
         console.log("E-Commerce DATABASE IS CONNECTED SUCCESSFULLY");
     })
     .catch(() =>{
-        console.log("DATABASE CONNECTION ERROR");
-    })                                                                                                                   
-    
-// const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://127.0.0.1:27017/E-Commerce')
-//     .then(() => {
-//         console.log("E-Commerce-Project DATABASE IS CONNECTED SUCCESSFULLY");
-//     })
-//     .catch(() =>{
-//         console.log("E-Commerce-Project DATABASE IS NOT CONNECTED");
-// })                                                                                                                              
+        console.log("E-Commerce DATABASE IS NOT CONNECTED");
+    })                                                                                                                                                                                                                                                
